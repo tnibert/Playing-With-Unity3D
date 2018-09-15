ecd2c2ef90bcbdba7943ac512da982d5aa2db65a
Tim Nibert <docz2a@gmail.com>
2018-09-15 21:04:57 +1000

    initial test c# script

---
+++ Assets/test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    // without assignment this int has a default value of 0
+    int frameCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		print("Up we GO");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        frameCount++;
+        print("And there's a frame: " + frameCount);
+	}
+
+    float GetDist(float x1, float y1, float x2, float y2) {
+        float dX = x2 - x1;
+        float dY = y2 - y1;
+        float dstSquared = dX * dX + dY * dY;
+        float dst = Mathf.Sqrt(dstSquared);
+        return dst;
+    }
+}
