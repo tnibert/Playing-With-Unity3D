5f7609e137b4d08379c57be6b689ea1be504edd1
Tim Nibert <docz2a@gmail.com>
2018-09-15 21:35:52 +1000

    created a simple timing game on the console

--- Assets/test.cs
+++ Assets/test.cs
@@ -6,16 +6,34 @@ public class test : MonoBehaviour {
 
     // without assignment this int has a default value of 0
     int frameCount = 0;
+    int target;
+    float start;
 
 	// Use this for initialization
 	void Start () {
 		print("Up we GO");
+        target = waitTime();
+        print("Initial target is " + target + "!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         frameCount++;
-        print("And there's a frame: " + frameCount);
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            float curTime = Time.time;
+            int diff = (int)(curTime - start);
+            print("target - " + target + ", diff - " + diff);
+            if(target == diff) {
+                print("EPIC WIN");
+            }
+            else {
+                print("FAILSAUCE");
+            }
+
+            target = waitTime();
+            print("NEW TARGET: " + target);
+        }
 	}
 
     float GetDist(float x1, float y1, float x2, float y2) {
@@ -24,5 +42,10 @@ public class test : MonoBehaviour {
         float dstSquared = dX * dX + dY * dY;
         float dst = Mathf.Sqrt(dstSquared);
         return dst;
+    }
+
+    int waitTime() {
+        start = Time.time;
+        return Random.Range(1,16);
     }
 }
