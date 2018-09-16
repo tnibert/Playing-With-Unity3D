98c703b8958d4da82ecd7fbdadbed98ea7ea98d5
Tim Nibert <docz2a@gmail.com>
2018-09-16 21:53:26 +1000

    Now have a ball chasing a cube in a 3D scene, added additional meta files

---
+++ Assets/Chaser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour {
+
+    public Transform targetTransform;
+    float speed = 7;
+
+    // update is called once per frame
+	void Update () {
+		Vector3 displace = targetTransform.position - transform.position;
+        Vector3 dirToTarget = displace.normalized;
+        Vector3 velocity = dirToTarget * speed;
+        
+        if(displace.magnitude > 1.5f) {
+            transform.Translate(velocity * Time.deltaTime);
+        }
+	}
+}
