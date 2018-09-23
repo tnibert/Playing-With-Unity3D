816affbe5ffd86a3a7a719282ab58b09fc5fa569
Tim Nibert <docz2a@gmail.com>
2018-09-23 11:35:37 +1000

    a bit more tinkering

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -9,11 +9,12 @@ public class CubeScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		sphereTransform.parent = transform;
+		sphereTransform.localScale = new Vector3(2, 2, 2);      // aka Vector3.one * 2
 	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.up * Time.deltaTime * 180, Space.World);
+		transform.Rotate(Vector3.up * Time.deltaTime * 90, Space.World);
 
 		// aka transform.eulerAngles += Vector3.down * 180 * Time.deltaTime;
 		// all of the following are equivalent:
@@ -22,5 +23,11 @@ public class CubeScript : MonoBehaviour {
 		// or Vector3.up * 180 * Time.deltaTime; - also have .down, .left, .right
 
 		//transform.Translate(Vector3.forward * Time.deltaTime * 7, Space.World);
+
+		// just for fun
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			sphereTransform.position = Vector3.zero;        // this is global space, so set position to absolute origin
+			                                                // if it were local space (e.g. sphereTransform.localPosition, it would set position to the position of the parent (cube)
+		}
 	}
 }
