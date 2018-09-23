0b414b330c2ffd05884ba5cefd429bf101e66e70
Tim Nibert <docz2a@gmail.com>
2018-09-23 15:13:18 +1000

    heyyy we have events

---
+++ Assets/GameUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour {
+
+	Player player;
+
+	// Use this for initialization
+	void Start () {
+		//GameObject playerObj = GameObject.Find("Player");
+		//player = playerObj.GetComponent<Player>();
+		player = FindObjectOfType<Player>();
+		player.onPlayerDeath += GameOver;     // subscribe GameOver function to onPlayerDeath event
+		player.onCoinGrab += DrawCoinCount;     // subscribe to onCoinGrab, we won't need this when we actually add a UI
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DrawCoinCount()
+	{
+		print(player.coinCount);
+	}
+
+	public void GameOver()
+	{
+		print("The player has died");
+	}
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
@@ -7,11 +8,15 @@ public class Player : MonoBehaviour {
 	public float speed = 6;
 	Rigidbody myRigidbody;      // for physics
 	Vector3 velocity;           // this default initializes to zero?
-	int coinCount = 0;
+	public int coinCount = 0;
+	public event Action onPlayerDeath;
+	public event Action onCoinGrab;
+	public int health = 3;
     
 	// Use this for initialization
 	void Start () {
 		myRigidbody = GetComponent<Rigidbody>(); 
+		// todo: let's decrement our health every collision
 	}
 
 	// Update is called once per frame
@@ -19,6 +24,16 @@ public class Player : MonoBehaviour {
 		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 		Vector3 direction = input.normalized;
 		velocity = direction * speed;
+
+		// check health status
+		if (health <= 0)
+		{
+			if (onPlayerDeath != null)
+			{
+				onPlayerDeath();
+			}
+			Destroy(gameObject);        // I suppose this is a reference to whatever the script is attached to
+		}
 	}
 
 	void FixedUpdate() {
@@ -28,12 +43,12 @@ public class Player : MonoBehaviour {
 	void OnTriggerEnter(Collider triggerCollider)
     // oh no he didn't
 	{
-		print(triggerCollider.gameObject.name);
+		//print(triggerCollider.gameObject.name);
 		if (triggerCollider.tag == "Coin")
 		{
 			Destroy(triggerCollider.gameObject);
 			coinCount++;
-			print(coinCount);
+			onCoinGrab();
 		}
 	}
 }
