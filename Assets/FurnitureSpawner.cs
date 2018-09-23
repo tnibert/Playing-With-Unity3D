03e71cb90b486bd905f331cdf59e8068dc697540
Tim Nibert <docz2a@gmail.com>
2018-09-23 14:14:07 +1000

    created a table prefab and now we spawn tables when we press p

---
+++ Assets/FurnitureSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureSpawner : MonoBehaviour {
+
+	public GameObject tablePrefab;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P)) {
+			Vector3 randomSpawnPos = new Vector3(Random.Range(-10f, 10f), 2, Random.Range(-10f, 10f));
+			Vector3 randomSpawnRot = Vector3.up * Random.Range(0, 360);
+
+            // why are these invisibile??? .. it was because the spawn position was placing the tables underneath our plane
+			GameObject newtable = (GameObject) Instantiate(tablePrefab, randomSpawnPos, Quaternion.Euler(randomSpawnRot));
+			newtable.transform.parent = transform;
+		}
+	}
+}
