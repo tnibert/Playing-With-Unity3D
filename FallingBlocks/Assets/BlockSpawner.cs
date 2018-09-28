97934bd08ffb11d1048f342fd35a4891291091b8
Tim Nibert <docz2a@gmail.com>
2018-09-28 16:52:03 +1000

    blocks are falling

---
+++ FallingBlocks/Assets/BlockSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+	public GameObject blockPrefab;
+	public float spawnDelaySec = 1;
+	float nextSpawn;
+	//float blockHalfHeight;
+    
+	public float minsize = 0.3f;
+	public float maxsize = 1.7f;
+    
+	float spawnAngleMax = 30f;
+
+	Vector2 screenHalfSize;
+    
+	// Use this for initialization
+	void Start () {
+		screenHalfSize = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+		//blockHalfHeight = blockPrefab.transform.localScale.x / 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSpawn)
+		{
+			float spawnAngle = Random.Range(-spawnAngleMax, spawnAngleMax); 
+			float spawnSize = Random.Range(minsize, maxsize);
+			Vector2 spawnPos = new Vector2(Random.Range(-screenHalfSize.x, screenHalfSize.x), screenHalfSize.y + hypotenuse(spawnSize));
+            
+			GameObject newBlock = (GameObject)Instantiate(blockPrefab, spawnPos, Quaternion.Euler(Vector3.forward * spawnAngle));        // Quaternion.identity means no rotation
+			newBlock.transform.localScale = Vector2.one * spawnSize;
+
+			nextSpawn = Time.time + spawnDelaySec;
+		}
+	}
+    
+	float hypotenuse(float spawnSize)
+	{
+		// a^2 + b^2 = c^2
+		float half = spawnSize / 2;
+		float result = Mathf.Sqrt((half * half) + (half * half));
+		return result;
+	}
+}
---
+++ FallingBlocks/Assets/FallingBlock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour {
+
+	public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.down * speed * Time.deltaTime);     // if we add Space.World it will not move in the direction of its rotation
+	}
+}
